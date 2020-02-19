1b9241ce1984cb3f7b4bd80225b861bbbe95d03d
nxthando <natedillamc@gmail.com>
2020-02-19 13:23:32 -0800

    Initial commit

---
+++ src/Book2.java
@@ -0,0 +1,12 @@
+public class Book2
+{
+    public int pages;
+    public Book2(int numPages)
+    {
+        pages=numPages;
+    }
+    public void pageMessage()
+    {
+        System.out.println("Number of pages: " + pages);
+    }
+}
